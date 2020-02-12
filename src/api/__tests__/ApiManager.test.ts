c83b51d6acec0a0a641e615a6a07ca7ebe529c61
Florent Tétaud <ftetaud@sodifrance.fr>
2020-02-12 17:23:46 +0100

    feat(US71 #1) : reprise nommage répertoire

--- src/api/__tests__/ApiManager.test.ts
+++ src/api/__tests__/ApiManager.test.ts
@@ -2,7 +2,7 @@ import { ApiManager } from "../ApiManager";
 import { isNullOrUndefined } from "util";
 
 test("instanciation d'une api définie dans le fichier api.json", () => {
-  const api = new ApiManager("rece-verificationctv-api", "v1");
+  const api = ApiManager.getInstance("rece-verificationctv-api", "v1");
 
   expect(api).not.toBe(isNullOrUndefined);
   expect(api.url).toBe("http://10.110.204.59");
@@ -15,9 +15,11 @@ test("instanciation d'une api définie dans le fichier api.json", () => {
 });
 
 test("instanciation d'une api définie dans le fichier api.json avec une mauvaise version", () => {
-  expect(() => new ApiManager("rece-verificationctv-api", "v3")).toThrow(Error);
+  expect(() =>
+    ApiManager.getInstance("rece-verificationctv-api", "v3")
+  ).toThrow(Error);
 });
 
 test("instanciation d'une api non définie dans le fichier api.json", () => {
-  expect(() => new ApiManager("rece-default-api", "v3")).toThrow(Error);
+  expect(() => ApiManager.getInstance("rece-default-api", "v3")).toThrow(Error);
 });
