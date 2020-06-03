cf6dd43cc9a9e68c7f8bf9abef554ac5e33c65ca
Bertrand Florat <bertrand.florat@ajtconseil.com>
2020-06-03 13:19:39 +0200

    ci: on remet les versions dans les URL d'ici reflexion

--- src/api/ApiManager.ts
+++ src/api/ApiManager.ts
@@ -92,7 +92,7 @@ export class ApiManager {
   }
 
   public getUri(): string {
-    return `${this.url}:${this.ports}/${this.name}`;
+    return `${this.url}:${this.ports}/${this.name}/${this.version}`;
   }
 
   public fetch(httpRequestConfig: HttpRequestConfig): Promise<any> {
