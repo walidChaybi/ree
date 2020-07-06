73c4b7b849c4f9a0d96c3bb30541c89a7b7929e8
vigourouxmi <mvigouroux@sodifrance.fr>
2020-07-06 16:02:19 +0200

    Feat(US 69) : Contenu de l'entête de l'application

--- src/views/core/header/__tests__/BoutonDeconnexion.test.tsx
+++ src/views/core/header/__tests__/BoutonDeconnexion.test.tsx
@@ -1,32 +1,37 @@
 import React from "react";
 import { BoutonDeconnexion } from "../BoutonDeconnexion";
 import { screen, render, fireEvent } from "@testing-library/react";
+import officier from "../../../../api/mock/officier.json";
 const ressource = require("../../../../ressources/ressource.json");
 
-test("renders bouton bouton utilisateur", () => {
-  const nom = "Garisson";
-  const prenom = "Juliette";
-  render(<BoutonDeconnexion nom={nom} prenom={prenom}></BoutonDeconnexion>);
+test("renders bouton utilisateur", () => {
+  render(
+    <BoutonDeconnexion
+      nom={officier.nom}
+      prenom={officier.prenom}
+    ></BoutonDeconnexion>
+  );
 
   const boutonElement = screen.getByText(/Juliette Garisson/i);
   expect(boutonElement).toBeInTheDocument();
 });
 
-test("renders click bouton déconnexion", () => {
+test("renders click bouton utilisateur", () => {
   const handleClickButton = jest.fn();
   render(
     <BoutonDeconnexion
-      nom=""
-      prenom=""
+      nom={officier.nom}
+      prenom={officier.prenom}
       onClick={handleClickButton}
     ></BoutonDeconnexion>
   );
 
+  const boutonElement = screen.getByText(/Juliette Garisson/i);
+  fireEvent.click(boutonElement);
+  expect(handleClickButton).toHaveBeenCalledTimes(1);
+
   if (ressource.boutonDeconnexion.visible) {
-    const boutonElement = screen.getByTestId("UtilisateurBouton");
-    fireEvent.click(boutonElement);
     const linkElement = screen.getByText(/Déconnexion/i);
-    fireEvent.click(linkElement);
-    expect(handleClickButton).toHaveBeenCalledTimes(1);
+    expect(linkElement).toBeInTheDocument();
   }
 });
