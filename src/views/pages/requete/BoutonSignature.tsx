5e942d138ce681aefb43d706f69d7e5f8185a081
le-biannicj <le-biannicj@wks-0329927>
2020-07-22 09:25:29 +0200

    appel webextension depuis la page délivrance en cours

--- src/views/pages/requete/BoutonSignature.tsx
+++ src/views/pages/requete/BoutonSignature.tsx
@@ -17,24 +17,48 @@ export const BoutonSignature: React.FC<BoutonSignatureProps> = ({
   libelle,
 }) => {
   const dialog = useDialogState();
+  const isError = false;
+
+  const handleClickSignature = () => {
+    let detail = {
+      function: "SIGN",
+      img: "image base64",
+      direction: "to-webextension",
+    };
+    window.top.dispatchEvent(new CustomEvent("signWebextCall", { detail }));
+    dialog.hide();
+  };
+
   return (
     <>
       <DialogDisclosure {...dialog} as={Button}>
         {getText(libelle)}
       </DialogDisclosure>
-      <Dialog
-        {...dialog}
-        tabIndex={0}
-        aria-label={getText("errors.dialogLabel")}
-        className="toast error"
-      >
-        <FontAwesomeIcon
-          icon={faTimesCircle}
-          title={getText("errors.icon")}
-          aria-label={getText("errors.icon")}
-        />
-        <Text messageId={"errors.pages.requetes.B01"} />
-      </Dialog>
+      {isError ? (
+        <Dialog
+          {...dialog}
+          tabIndex={0}
+          aria-label={getText("errors.dialogLabel")}
+          className="toast error"
+        >
+          <FontAwesomeIcon
+            icon={faTimesCircle}
+            title={getText("errors.icon")}
+            aria-label={getText("errors.icon")}
+          />
+          <Text messageId={"errors.pages.requetes.B01"} />
+        </Dialog>
+      ) : (
+        <Dialog
+          {...dialog}
+          tabIndex={0}
+          aria-label={getText("errors.dialogLabel")}
+          className="toast"
+        >
+          <Text messageId="signature.confirmation" />
+          <Button onClick={handleClickSignature}>OK</Button>
+        </Dialog>
+      )}
     </>
   );
 };
