9862419f467c882105a1cb0d721d5039ba0a0ccb
fboncler <fboncler@sodifrance.fr>
2020-12-04 09:39:06 +0100

    feat(us234): ajout TU

--- src/__tests__/common/widget/accordion/AccordionContent.test.tsx
+++ src/__tests__/common/widget/accordion/AccordionContent.test.tsx
@@ -5,7 +5,7 @@ import { AccordionContent } from "../../../../views/common/widget/accordion/Acco
 
 test("render composant AccordionContent", async () => {
   const { getByText } = render(
-    <AccordionContent libelle="testLibelle" value="testValue" />
+    <AccordionContent libelle="testLibelle" value="testValue" row={0} />
   );
 
   await waitFor(() => {
