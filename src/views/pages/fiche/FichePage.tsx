3188c94f20989d0997d744897b84437e31a04080
Hacquart Nicolas <nicolas.hacquart@soprasteria.com>
2021-01-28 14:37:53 +0100

    feat: us301 : ajout du bandeau d'alerte avec le bouton d'ajout d'alerte grisé

--- src/views/pages/fiche/FichePage.tsx
+++ src/views/pages/fiche/FichePage.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react";
-import { BandeauFiche } from "./contenu/BandeauFiche";
-import { useFichePageApiHook } from "./hook/FichePageApiHook";
-import { AccordionRece } from "../../common/widget/accordion/AccordionRece";
-import { FenetreExterneUtil } from "../../common/util/FenetreExterne";
-import { BandeauFicheRcRcaPacsNumero } from "./contenu/BandeauFicheRcRcaPacsNumero";
-import { BandeauFicheActeNumero } from "./contenu/BandeauFicheActeNumero";
-import { FicheUtil } from "../../../model/etatcivil/TypeFiche";
+import React, {useEffect} from "react";
+import {BandeauFiche} from "./contenu/BandeauFiche";
+import {useFichePageApiHook} from "./hook/FichePageApiHook";
+import {AccordionRece} from "../../common/widget/accordion/AccordionRece";
+import {FenetreExterneUtil} from "../../common/util/FenetreExterne";
+import {BandeauFicheRcRcaPacsNumero} from "./contenu/BandeauFicheRcRcaPacsNumero";
+import {BandeauFicheActeNumero} from "./contenu/BandeauFicheActeNumero";
+import {FicheUtil} from "../../../model/etatcivil/TypeFiche";
+import {AlerteActe} from "./hook/constructionComposants/acte/AlerteActeUtils";
 
 export interface FichePageProps {
   dataFiche: IDataFicheProps;
@@ -41,28 +42,30 @@ export const FichePage: React.FC<FichePageProps> = props => {
   }, [dataFicheState, props.fenetreExterneUtil]);
 
   return (
-    <div>
-      {/* Le Bandeau */}
-      {dataFicheState.dataBandeau && (
-        <BandeauFiche
-          dataBandeau={dataFicheState.dataBandeau}
-          elementNumeroLigne={getElementNumeroLigne()}
-        ></BandeauFiche>
-      )}
-      {/* Les Accordéons */}
-      {dataFicheState.fiche && <AccordionRece {...dataFicheState.fiche} />}
-    </div>
+      <div>
+        {/* Le Bandeau */}
+        {dataFicheState.dataBandeau && (
+            <BandeauFiche
+                dataBandeau={dataFicheState.dataBandeau}
+                elementNumeroLigne={getElementNumeroLigne()}
+            />
+        )}
+        {/* Le bandeau d'ajout d'alerte pour les actes */}
+        {FicheUtil.isActe(props.dataFiche.categorie) && <AlerteActe/>}
+        {/* Les Accordéons */}
+        {dataFicheState.fiche && <AccordionRece {...dataFicheState.fiche} />}
+      </div>
   );
 
   function getElementNumeroLigne() {
     return FicheUtil.isActe(props.dataFiche.categorie) ? (
-      <BandeauFicheActeNumero
-        dataBandeau={dataFicheState.dataBandeau}
-      ></BandeauFicheActeNumero>
-    ) : (
-      <BandeauFicheRcRcaPacsNumero
-        dataBandeau={dataFicheState.dataBandeau}
-      ></BandeauFicheRcRcaPacsNumero>
+        <BandeauFicheActeNumero
+            dataBandeau={dataFicheState.dataBandeau}
+        />) : (
+        <BandeauFicheRcRcaPacsNumero
+            dataBandeau={dataFicheState.dataBandeau}
+        />
     );
   }
 };
+
