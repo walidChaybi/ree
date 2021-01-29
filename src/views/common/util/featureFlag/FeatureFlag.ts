7358a3ed9b4dd5c8575ea9a62700947172376ca7
le-biannicj <jlebiannic@sodifrance.fr>
2021-01-29 14:52:30 +0100

    Maj ReadMe avec Feature flags

--- src/views/common/util/featureFlag/FeatureFlag.ts
+++ src/views/common/util/featureFlag/FeatureFlag.ts
@@ -1,7 +1,7 @@
-// ID          | Fonction                                |  Statut | Date peremption?
+// ID          | Fonction                                |  Statut | Date peremption
 // ------------|-----------------------------------------|---------|-------------------
-// ETAPE2      | Conditionne les features de l'étape2    | INACTIF | ?
-// LOG_SERVEUR | Conditionne l'envoi des logs au serveur | INACTIF | ?
+// ETAPE2      | Conditionne les features de l'étape2    | INACTIF | Lorsque l'étape 1 sera terminée
+// LOG_SERVEUR | Conditionne l'envoi des logs au serveur | INACTIF | Lorsque l'étape 1 sera terminée
 
 export enum FeatureFlag {
   ETAPE2 = "ETAPE2",
