478a0d01e4c81152a0ea662cca350cc5baeac8d8
le-biannicj <jlebiannic@sodifrance.fr>
2021-06-17 07:52:28 +0200

    feat: detection navigateur

--- src/views/core/App.tsx
+++ src/views/core/App.tsx
@@ -1,25 +1,30 @@
 /* istanbul ignore file */
 
+import fr from "date-fns/locale/fr";
 import React from "react";
-import "../../scss/_library.scss";
-import "../../scss/_colors.scss";
-import "./App.scss";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Header } from "./header/Header";
-import { Body } from "./body/Body";
 import { ToastContainer } from "react-toastify";
-import { useLoginApi } from "./login/LoginHook";
-import { OfficierContext } from "./contexts/OfficierContext";
+import "../../scss/_colors.scss";
+import "../../scss/_library.scss";
 import {
-  GestionnaireFermeture,
+  CHROME,
+  FIREFOX,
+  SeulementNavigateur
+} from "../common/util/detectionNavigateur/DetectionNavigateur";
+import { ErrorManager } from "../common/util/ErrorManager";
+import {
   appelRequetesASigner,
+  GestionnaireFermeture,
   traiteAppelRequeteASigner
 } from "../common/util/GestionnaireFermeture";
-import { URL_MES_REQUETES } from "../router/ReceUrls";
 import { storeRece } from "../common/util/storeRece";
-import { ErrorManager } from "../common/util/ErrorManager";
-import { registerLocale, setDefaultLocale } from "react-datepicker";
-import fr from "date-fns/locale/fr";
+import { URL_MES_REQUETES } from "../router/ReceUrls";
+import "./App.scss";
+import { Body } from "./body/Body";
+import { OfficierContext } from "./contexts/OfficierContext";
+import { Header } from "./header/Header";
+import { useLoginApi } from "./login/LoginHook";
 // ReceDatepicker Locale
 registerLocale("fr", fr);
 setDefaultLocale("fr");
@@ -28,42 +33,48 @@ const App: React.FC = () => {
   const login = useLoginApi();
 
   return (
-    <ErrorManager>
-      <Router>
-        <div className="App">
-          <OfficierContext.Provider value={login}>
-            <OfficierContext.Consumer>
-              {officier => {
-                storeRece.utilisateurCourant = officier?.officierDataState;
-                return null;
-              }}
-            </OfficierContext.Consumer>
-            <OfficierContext.Consumer>
-              {officier => (
-                <GestionnaireFermeture
-                  paramsFctAAppler={officier}
-                  fctAAppeler={appelRequetesASigner}
-                  fctTraitementResultat={traiteAppelRequeteASigner}
-                  urlRedirection={URL_MES_REQUETES}
-                ></GestionnaireFermeture>
-              )}
-            </OfficierContext.Consumer>
-            <Header />
-            <Body />
-            <ToastContainer
-              className={"toast-container"}
-              position="top-center"
-              hideProgressBar={false}
-              newestOnTop={true}
-              closeOnClick={true}
-              rtl={false}
-              draggable={true}
-              pauseOnHover={true}
-            />
-          </OfficierContext.Provider>
-        </div>
-      </Router>
-    </ErrorManager>
+    <SeulementNavigateur
+      navigateurs={
+        process.env.NODE_ENV === "development" ? [FIREFOX, CHROME] : [FIREFOX]
+      }
+    >
+      <ErrorManager>
+        <Router>
+          <div className="App">
+            <OfficierContext.Provider value={login}>
+              <OfficierContext.Consumer>
+                {officier => {
+                  storeRece.utilisateurCourant = officier?.officierDataState;
+                  return null;
+                }}
+              </OfficierContext.Consumer>
+              <OfficierContext.Consumer>
+                {officier => (
+                  <GestionnaireFermeture
+                    paramsFctAAppler={officier}
+                    fctAAppeler={appelRequetesASigner}
+                    fctTraitementResultat={traiteAppelRequeteASigner}
+                    urlRedirection={URL_MES_REQUETES}
+                  ></GestionnaireFermeture>
+                )}
+              </OfficierContext.Consumer>
+              <Header />
+              <Body />
+              <ToastContainer
+                className={"toast-container"}
+                position="top-center"
+                hideProgressBar={false}
+                newestOnTop={true}
+                closeOnClick={true}
+                rtl={false}
+                draggable={true}
+                pauseOnHover={true}
+              />
+            </OfficierContext.Provider>
+          </div>
+        </Router>
+      </ErrorManager>
+    </SeulementNavigateur>
   );
 };
 
