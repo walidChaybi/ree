5a24458a929a901c959c42881c8713f5b4d91c1f
Benoit Vrignaud <benoit.vrignaud@eole-consulting.fr>
2022-01-25 10:16:43 +0100

    fix 91484

--- src/views/pages/fiche/hook/constructionComposants/interesses/Mariage.tsx
+++ src/views/pages/fiche/hook/constructionComposants/interesses/Mariage.tsx
@@ -11,14 +11,8 @@ import "./scss/Mariage.scss";
 export const Mariage: React.FC<IMariageInteresse> = props => {
   return (
     <>
-      <span>
-        {
-          <label className="libelleContent">
-            {getLibelleLieuMariage(props)}
-          </label>
-        }
-        <span className="valueContent mariage">{getLieuMariage(props)}</span>
-      </span>
+      {<label className="libelleContent">{getLibelleLieuMariage(props)}</label>}
+      <span className="valueContent mariage">{getLieuMariage(props)}</span>
       <span>
         {
           <label className="libelleContent">
