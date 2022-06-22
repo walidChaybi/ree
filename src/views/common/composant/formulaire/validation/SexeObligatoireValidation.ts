99928d93edd18580a31023b84cbcc8e4c6936554
le-biannicj <jlebiannic@sodifrance.fr>
2022-06-22 10:43:27 +0200

    fix: sexe obligatoire seulement si le nom du parent est saisi

--- src/views/common/composant/formulaire/validation/SexeObligatoireValidation.ts
+++ src/views/common/composant/formulaire/validation/SexeObligatoireValidation.ts
@@ -1,16 +1,18 @@
 import { Sexe } from "../../../../../model/etatcivil/enum/Sexe";
+import { IParentNaissanceForm } from "../../../../pages/requeteDelivrance/editionExtraitCopie/contenu/onglets/saisirExtrait/mapping/mappingActeVerFormulaireSaisirExtrait";
 import { getLibelle } from "../../../util/Utils";
 import { SEXE } from "../ConstantesNomsForm";
 
 export const sexeObligatoireValidation = function (
   context: any,
-  value: any,
+  value: IParentNaissanceForm,
   error: any
 ) {
   let res: any = true;
   if (
     value &&
-    (!value[SEXE] || Sexe.getEnumFor(value[SEXE]) === Sexe.INCONNU)
+    value.nomNaissance &&
+    (!value.sexe || Sexe.getEnumFor(value.sexe) === Sexe.INCONNU)
   ) {
     const paramsError = {
       path: `${error.path}.${SEXE}`,
