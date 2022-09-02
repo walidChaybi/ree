34270b7ae8f7a0319fed4fcc0d23ca630a31d70d
Benoit Vrignaud <benoit.vrignaud@eole-consulting.fr>
2022-09-02 15:14:33 +0200

    test

--- src/model/etatcivil/acte/IFicheActe.ts
+++ src/model/etatcivil/acte/IFicheActe.ts
@@ -286,8 +286,7 @@ function extraireMentionNationalite(texte?: string) {
       regex =
         /(Français par|Française par)[ :\r\n]+([\wÀ-ÿ '°(.\r\n)]*)sous l'identité de[\wÀ-ÿ '°.\r\n]*(\([\wÀ-ÿ '°(.\r\n)]*\))\r?\n\r?\n/gm;
     } else {
-      regex =
-        /(Français par|Française par)[ :\r\n]+([\wÀ-ÿ\-, '°(.\r\n)]*?)\r?\n\r?\n/gm;
+      regex = /(Français par|Française par)[:\s]+([\s\w\W]*?)\r?\n\r?\n/gm;
     }
 
     const matches = new RegExp(regex).exec(texte);
