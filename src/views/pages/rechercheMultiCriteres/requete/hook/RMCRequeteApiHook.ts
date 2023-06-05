d5402d059c3c69eb2a294dd56042709bffff9879
Jules Girard <jules.girard-ext@diplomatie.gouv.fr>
2023-06-05 12:42:56 +0000

    fix: Gestion operation en cours à false quand fin de RMC Requete incorrecte.

--- src/views/pages/rechercheMultiCriteres/requete/hook/RMCRequeteApiHook.ts
+++ src/views/pages/rechercheMultiCriteres/requete/hook/RMCRequeteApiHook.ts
@@ -4,43 +4,47 @@ import { ICriteresRMCRequete } from "@model/rmc/requete/ICriteresRMCRequete";
 import { getParamsTableau, IParamsTableau } from "@util/GestionDesLiensApi";
 import { logError } from "@util/LogManager";
 import { mappingRequetesTableau } from "@util/RequetesUtils";
+import { execute, getLibelle } from "@util/Utils";
 import { useEffect, useState } from "react";
 import { mappingCriteresRequete } from "./RMCRequeteMapping";
 
-export function useRMCRequeteApiHook(criteres?: ICriteresRMCRequete) {
+export function useRMCRequeteApiHook(criteresRMCRequete?: ICriteresRMCRequete) {
   const [dataRMCRequete, setDataRMCRequete] = useState<TRequeteTableau[]>();
   const [dataTableauRMCRequete, setDataTableauRMCRequete] =
     useState<IParamsTableau>();
 
   useEffect(() => {
-    async function fetchRequetes() {
-      try {
-        if (criteres != null && criteres.valeurs != null) {
-          const criteresRequest = mappingCriteresRequete(criteres.valeurs);
-          const result = await rechercheMultiCriteresRequetes(
-            criteresRequest,
-            criteres.range
-          );
+    if (criteresRMCRequete && criteresRMCRequete.valeurs) {
+      const criteres = mappingCriteresRequete(criteresRMCRequete.valeurs);
+      rechercheMultiCriteresRequetes(criteres, criteresRMCRequete.range)
+        .then((result: any) => {
           const requetes = mappingRequetesTableau(
             result?.body?.data?.resultatsRecherche,
             true
           );
           setDataRMCRequete(requetes);
           setDataTableauRMCRequete(getParamsTableau(result));
-        }
-      } catch (error) {
-        logError({
-          messageUtilisateur:
-            "Impossible de récupérer les requetes de la recherche multi-critères",
-          error
+          if (!result?.body) {
+            // TODO: Temporaire, à modifier quand le back renverra le TimeoutException correctement.
+            throw new Error(
+              getLibelle(
+                "La réponse du serveur est trop longue, veuillez affiner les critères de recherche."
+              )
+            );
+          }
+        })
+        .catch((error: any) => {
+          /* istanbul ignore next */
+          logError({
+            messageUtilisateur: getLibelle(
+              "Impossible de récupérer les requetes de la recherche multi-critères"
+            ),
+            error
+          });
+          execute(criteresRMCRequete?.onErreur);
         });
-        if (criteres?.onErreur) {
-          criteres.onErreur();
-        }
-      }
     }
-    fetchRequetes();
-  }, [criteres]);
+  }, [criteresRMCRequete]);
 
   return {
     dataRMCRequete,
