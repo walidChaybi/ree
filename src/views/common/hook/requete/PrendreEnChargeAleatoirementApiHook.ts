466605aac07ab3065ae6303599aded48af703cab
Gabriel Brian Grebot <gabriel.brian-grebot-ext@diplomatie.gouv.fr>
2025-07-21 17:10:03 +0200

    fix(STRECE-7433): message erreur requete aleatoire

--- src/views/common/hook/requete/PrendreEnChargeAleatoirementApiHook.ts
+++ src/views/common/hook/requete/PrendreEnChargeAleatoirementApiHook.ts
@@ -12,45 +12,24 @@ export interface IRequeteAleatoireResultat {
   requete: TRequeteTableau;
 }
 
-export function useGetRequeteAleatoire(
-  type: TypeRequete,
-  prendreEnCharge: boolean
-) {
+export function useGetRequeteAleatoire(type: TypeRequete, prendreEnCharge: boolean) {
   const { utilisateurs, services } = useContext(RECEContextData);
-  const [requeteAleatoireResultat, setRequeteAleatoireResultat] = useState<
-    IRequeteAleatoireResultat | undefined
-  >();
+  const [requeteAleatoireResultat, setRequeteAleatoireResultat] = useState<IRequeteAleatoireResultat | undefined>();
   useEffect(() => {
     async function fetchRequeteAleatoire() {
       try {
         if (prendreEnCharge) {
           switch (type) {
             case TypeRequete.DELIVRANCE:
-              const resultatDel = await getRequeteAleatoire(
-                TypeRequete.DELIVRANCE.nom
-              );
-              const requeteResultatDelMappee =
-                mappingUneRequeteTableauDelivrance(
-                  resultatDel.body.data,
-                  false,
-                  utilisateurs,
-                  services
-                );
+              const resultatDel = await getRequeteAleatoire(TypeRequete.DELIVRANCE.nom);
+              const requeteResultatDelMappee = mappingUneRequeteTableauDelivrance(resultatDel.body.data, false, utilisateurs, services);
               setRequeteAleatoireResultat({
                 requete: requeteResultatDelMappee
               });
               break;
             case TypeRequete.INFORMATION:
-              const resultatInfo = await getRequeteAleatoire(
-                TypeRequete.INFORMATION.nom
-              );
-              const requeteResultatInfoMappee =
-                mappingUneRequeteTableauInformation(
-                  resultatInfo.body.data,
-                  false,
-                  utilisateurs,
-                  services
-                );
+              const resultatInfo = await getRequeteAleatoire(TypeRequete.INFORMATION.nom);
+              const requeteResultatInfoMappee = mappingUneRequeteTableauInformation(resultatInfo.body.data, false, utilisateurs, services);
               setRequeteAleatoireResultat({
                 requete: requeteResultatInfoMappee
               });
@@ -68,14 +47,11 @@ export function useGetRequeteAleatoire(
   return requeteAleatoireResultat;
 }
 
-export function gereErreur(error: any, setRequeteAleatoireResultat: any) {
-  if (error.response && error.response.status === HTTP_NOT_FOUND) {
-    setRequeteAleatoireResultat({ requete: undefined });
-  } else {
-    logError({
-      messageUtilisateur:
-        "Impossible de prendre en charge aléatoirement une requête",
-      error
-    });
-  }
-}
+export const gereErreur = (error: any, setRequeteAleatoireResultat: any) => {
+  error.response?.status === HTTP_NOT_FOUND
+    ? setRequeteAleatoireResultat?.({ requete: undefined })
+    : logError({
+        messageUtilisateur: "Impossible de prendre en charge une requête",
+        error
+      });
+};
