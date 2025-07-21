be1233d0cfbfeb5c4dbe2fd026bec9579a23803f
Gabriel BRIAN-GREBOT <gabriel.brian-grebot-ext@diplomatie.gouv.fr>
2025-07-21 13:28:49 +0200

    feat(STRECE-8133): coller nom complet titulaire rmc

--- src/__tests__/composants/commun/champs/ChampsNomPrenomInterchangeables.test.tsx
+++ src/__tests__/composants/commun/champs/ChampsNomPrenomInterchangeables.test.tsx
@@ -1,4 +1,5 @@
-import { act, fireEvent, render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Formik } from "formik";
 import { describe, expect, test } from "vitest";
 import { ChampsNomPrenomInterchangeables } from "../../../../composants/commun/champs/ChampsNomPrenomInterchangeables";
@@ -25,10 +26,56 @@ describe("Test du composant Champs Nom Prenom Interchangeables", () => {
     expect(champNom.value).toBe("Son");
     expect(champPrenom.value).toBe("Goku");
 
-    const boutonReinitialiser: HTMLButtonElement = screen.getByTitle("Intervertir Nom et Prénom");
-    fireEvent.click(boutonReinitialiser);
+    const boutonIntervertir: HTMLButtonElement = screen.getByTitle("Intervertir Nom et Prénom");
+    fireEvent.click(boutonIntervertir);
 
     expect(champNom.value).toBe("Goku");
     expect(champPrenom.value).toBe("Son");
+  });
+
+  test("Doit gérer le collage d'un nom complet", async () => {
+    await act(async () => afficherFormulaire());
+    const champNom: HTMLInputElement = screen.getByLabelText("Nom");
+    const champPrenom: HTMLInputElement = screen.getByLabelText("Prénom");
+
+    // Doit placer les vocables en majuscule dans nom et celles en minuscule dans prenom
+    await userEvent.click(champNom);
+    await userEvent.paste("NOM1 Prenom1");
+
+    await waitFor(() => {
+      expect(champNom.value).toBe("NOM1");
+      expect(champPrenom.value).toBe("Prenom1");
+    });
+
+    await userEvent.click(champNom);
+    await userEvent.paste("Prenom1 NOM1");
+
+    await waitFor(() => {
+      expect(champNom.value).toBe("NOM1");
+      expect(champPrenom.value).toBe("Prenom1");
+    });
+
+    await userEvent.click(champNom);
+    await userEvent.paste("NOM1 NOM-2 N'OM3 Prenom1 Prenom2 Pre-nom3");
+
+    await waitFor(() => {
+      expect(champNom.value).toBe("NOM1 NOM-2 N'OM3");
+      expect(champPrenom.value).toBe("Prenom1 Prenom2 Pre-nom3");
+    });
+
+    // Ne DOIT PAS intervenir si les deux groupes de vocable ne sont pas clairement séparés
+    champNom.value = "";
+    champPrenom.value = "";
+
+    await userEvent.clear(champNom);
+    await userEvent.clear(champPrenom);
+
+    await userEvent.click(champNom);
+    await userEvent.paste("NOM1 NOM-2 N'OM3 Prenom1 NOM 4 Prenom2 Pre-nom3");
+
+    await waitFor(() => {
+      expect(champNom.value).toBe("NOM1 NOM-2 N'OM3 Prenom1 NOM 4 Prenom2 Pre-nom3");
+      expect(champPrenom.value).toBe("");
+    });
   });
 });
--- src/composants/commun/champs/ChampsNomPrenomInterchangeables.tsx
+++ src/composants/commun/champs/ChampsNomPrenomInterchangeables.tsx
@@ -11,12 +11,38 @@ export const ChampsNomPrenomInterchangeables: React.FC<{ cheminNom: string; chem
     setFieldValue(cheminPrenom, getIn(values, cheminNom)).finally(() => setFieldTouched(cheminPrenom, true));
   };
 
+  const placerNomEtPrenomDansLesBonsChamps = (event: React.ClipboardEvent<HTMLInputElement>): void => {
+    const vocables: string[] = event.clipboardData.getData("text").split(" ");
+    const estVocableEnMajuscule: boolean[] = vocables
+      .map(vocable => (vocable ? vocable === vocable.toLocaleUpperCase() : null))
+      .filter((valeur): valeur is boolean => valeur !== null);
+
+    if (estVocableEnMajuscule.length < 2) return;
+
+    const indexPremierVocableAuFormatDifferent = estVocableEnMajuscule.findIndex(enMajuscule => enMajuscule !== estVocableEnMajuscule[0]);
+
+    if (indexPremierVocableAuFormatDifferent === -1) return;
+
+    const vocablesAuxFormatsDifferents = estVocableEnMajuscule.slice(indexPremierVocableAuFormatDifferent);
+
+    if (vocablesAuxFormatsDifferents.every(vocable => vocable === vocablesAuxFormatsDifferents[0])) {
+      event.preventDefault();
+
+      const premierePartie = vocables.slice(0, indexPremierVocableAuFormatDifferent).join(" ");
+      const secondePartie = vocables.slice(indexPremierVocableAuFormatDifferent).join(" ");
+
+      setFieldValue(cheminNom, estVocableEnMajuscule[0] ? premierePartie : secondePartie);
+      setFieldValue(cheminPrenom, estVocableEnMajuscule[0] ? secondePartie : premierePartie);
+    }
+  };
+
   return (
     <div className="grid grid-cols-2 gap-4 pt-4">
       <div className="flex gap-4">
         <ChampTexte
           name={cheminNom}
           libelle="Nom"
+          onPaste={placerNomEtPrenomDansLesBonsChamps}
         />
         <BoutonIcon
           title="Intervertir Nom et Prénom"
