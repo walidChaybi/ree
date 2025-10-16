b23cd33eef58f38b8fec63076d9e610619863e65
peans <sofiane.pean-ext@diplomatie.gouv.fr>
2025-10-16 10:40:56 +0200

    feat(STRECE-8633): Mise à jour des habilitations pour la saisie des requêtes

--- src/api/configurations/etatCivil/pocopa/GetPostesConfigApi.ts
+++ src/api/configurations/etatCivil/pocopa/GetPostesConfigApi.ts
@@ -2,7 +2,7 @@ import { ETATCIVIL_API } from "@api/ApiDisponibles";
 import { TConfigurationApi } from "@model/api/Api";
 import { ITypeRegistreDto } from "@model/etatcivil/acte/TypeRegistre";
 
-const URI = "/types-registres/pocopas";
+const URI = "/types-registres/transcription/postes";
 
 export const CONFIG_GET_POSTES: TConfigurationApi<typeof URI, undefined, undefined, ITypeRegistreDto[] | []> = {
   api: ETATCIVIL_API,
